e20546146fd6d674a6b6ee02d3363d224d81df5f
Celine <126262323+CelineHarakee@users.noreply.github.com>
2024-01-24 15:40:03 +0300

    Add files via upload
    Use stacks to reverse a sentence.

---
+++ Stack1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stack1{
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String input = scan.nextLine();
+
+        String words[] = input.split(" "); // built in function, whenever it finds a space, it cuts it
+
+        Stack<String> s = new Stack<String>();
+        
+        for (int i=0;i<words.length;i++) {
+            s.push(words[i]);
+        }
+
+        for (int i = 0 ; i < words.length ; i++){
+            System.out.println(s.pop() + " ");
+        }
+
+    }
+}
