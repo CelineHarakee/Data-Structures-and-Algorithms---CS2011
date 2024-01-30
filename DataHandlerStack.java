756b16fc7a7b525e9219bc5ac251ba7c759c785d
Celine <126262323+CelineHarakee@users.noreply.github.com>
2024-01-30 22:56:56 +0300

    Add files via upload

---
+++ DataHandlerStack.java
@@ -0,0 +1,45 @@
+//  The purpose of this program is to demonstrate the use of a stack in handling text files.
+
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+
+
+public class DataHandlerStack {
+    public String DataHandler (String fileName) throws FileNotFoundException {
+            String originalData = "";
+            String reversedData = "";
+
+            Stack<String> stack = new Stack<String>();
+
+            File myFile = new File(fileName);
+
+            Scanner myScanner = new Scanner(myFile);
+            while (myScanner.hasNextLine()){
+                String line = myScanner.nextLine();
+                stack.push(line);
+                originalData += line + "\n";
+            }
+        
+            myScanner.close();
+
+            while (!stack.isEmpty()){
+                reversedData += stack.pop() + "\n";
+            }
+
+            return "Original data: \n" + originalData + "Reversed data: " + reversedData;
+        }
+    
+        public static void main(String[] args) {
+            
+        DataHandlerStack dh = new DataHandlerStack();
+        try{
+            String data = dh.DataHandler("text.txt");
+            System.out.println(data);
+        }
+
+        catch(FileNotFoundException e){
+            System.out.println("An error occured, file was not found");
+        }
+    }
+}
