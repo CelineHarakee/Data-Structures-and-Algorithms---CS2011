ad2ebff7770b4b7a04be54700b065b224bdabc65
Celine <126262323+CelineHarakee@users.noreply.github.com>
2024-02-21 08:18:21 +0300

    Update ArrayListStack.java

--- ArrayListStack.java
+++ ArrayListStack.java
@@ -3,30 +3,79 @@
 import java.util.*;
 
 public class ArrayListStack {
+    Object arr[];
+    int n;
 
-    public String reverse (String input){
+    public ArrayListStack() {
+        arr = new Object[10];
+        n = 0;
 
-        Stack<Character> stack = new Stack<Character>();
-        char[] charArray = input.toCharArray();
-
-        
-        for (int i = 0 ; i < charArray.length ; i++){
-            stack.push(charArray[i]);
+    }
+    public void add(int i, Object x){
+        if(n == arr.length){
+            resize();
         }
-        
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
+
+        for(int j = n; j > i ;j--) {
+            arr[j] = arr[j-1];
         }
+        arr[i] = x;
+        n++;
+    }
+
+    public Object remove(int i){
+
+        Object x = arr[i];
+        for(int j = i; j < n -1;j++) {
+            arr[j] = arr[j+1];
+        }
+        n--;
+
+        if(n < arr.length){
+            resize();
+        }
+        return x;
+    }
+
+    public void resize(){
+        Object arr2[] = new Object[n*2];
+
+        for(int i = 0; i < n;i++){
+            arr2[i] = arr[i];
+        }
+        arr = arr2;
+    }
+
+        public void push (Object x)
+        {
+            add (n,x);
+        }
+
+        public Object pop()
+        {
+            return remove (n - 1);
+        }
+
+        public void separating (String inputString){
+            
+            String[] word  = inputString.split(" ");
+
+            for (int i = 0 ; i < word.length ; i ++){
+                push(word[i]);
+            }
+
+            for (int i = 0 ; i < word.length ; i ++){
+                System.out.print(pop() + " ");
+            }
+        }
+
+        public static void main(String[] args) {
+
+            String inputString = "My name is Celine";
+
+            ArrayListStack arrayList = new ArrayListStack();
     
-        return reversed;
-    }
+            arrayList.separating(inputString);
+        }
 
-    public static void main(String[] args) {
-        ArrayListStack arrayList = new ArrayListStack();
-        String input = "Celine";
-        String reversed = arrayList.reverse(input);
-        System.out.println("Original String: " + input);
-        System.out.println("Reversed string: " + reversed);
-    }
 }
